7cc72ba79da8d466b0ffc00c2fa1a395bce4318b
abhishekbharti <abhishek.bharti@testbook.com>
2019-04-27 18:04:23 +0530

    bootstrap fixes and static files added

--- abhishekBhartiProjects/home/views.py
+++ abhishekBhartiProjects/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 
-def index(request):
-    return render(request, "home/index.html")
 
+def index(request):
+    context = {
+        'title': 'Home'
+    }
+
+    return render(request, "home/index.html", context)
